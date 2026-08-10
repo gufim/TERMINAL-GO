38996cef5f4bf301072a903376d48bb21257a0f5
gufim <65247648+gufim@users.noreply.github.com>
2026-08-10 18:45:44 +0100

    test

--- src/bench_test.go
+++ src/bench_test.go
@@ -15,3 +15,17 @@ func BenchmarkNewCustomEntry(b *testing.B) {
 		_ = i
 	}
 }
+
+func BenchmarkSaveSettingsToFile(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		// TODO: wywołaj saveSettingsToFile(...)
+		_ = i
+	}
+}
+
+func BenchmarkCreateBlinker(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		// TODO: wywołaj createBlinker(...)
+		_ = i
+	}
+}
